d6f32208d960cbda4ec8be008e353c02c3b7673b
JeanpaulDAW <jean.milachay04@gmail.com>
2025-11-11 11:06:32 +0100

    Update trabajonumpy.py

--- trabajonumpy.py
+++ trabajonumpy.py
@@ -2,4 +2,4 @@
 import matplotlib.pyplot as plt
 import numpy as np
 #leemos el iris, cargamos a matriz, descartamos primera fila, separador ,
-bombeiro = np.loadtxt('.ActuacionesBomberos_2025.csv', skiprows=1, delimiter=';',)
+bombero = np.loadtxt('.ActuacionesBomberos_2025.csv', skiprows=1, delimiter=';',)
