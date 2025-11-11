a78b1d2ba3c98da2d27dcf93bf3d915393a0deb2
ACepedaIglesias <acepedaiglesias@gmail.com>
2025-11-11 11:05:32 +0100

    Initialize trabajonumpy.py with imports and data load

---
+++ trabajonumpy.py
@@ -0,0 +1,5 @@
+# Para el trabajo
+import matplotlib.pyplot as plt
+import numpy as np
+#leemos el iris, cargamos a matriz, descartamos primera fila, separador ,
+bombeiro = np.loadtxt('.ActuacionesBomberos_2025.csv', skiprows=1, delimiter=';',)
