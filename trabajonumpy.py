a52e17e54bfd7ccf2f14502f1aa5f94e0ef59e03
JeanpaulDAW <jean.milachay04@gmail.com>
2025-11-11 11:07:47 +0100

    Update trabajonumpy.py

--- trabajonumpy.py
+++ trabajonumpy.py
@@ -4,3 +4,4 @@ import numpy as np
 #leemos el iris, cargamos a matriz, descartamos primera fila, separador ,
 bombero = np.loadtxt('.ActuacionesBomberos_2025.csv', skiprows=1, delimiter=';',)
 FENTANILO
+PERRO SANCHEZ
