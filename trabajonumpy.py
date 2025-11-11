843d1e1e745e2b0e7cf8cf2283b1be948542c811
ACepedaIglesias <acepedaiglesias@gmail.com>
2025-11-11 11:08:48 +0100

    putilla

--- trabajonumpy.py
+++ trabajonumpy.py
@@ -2,6 +2,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 #leemos el iris, cargamos a matriz, descartamos primera fila, separador ,
-bombero = np.loadtxt('.ActuacionesBomberos_2025.csv', skiprows=1, delimiter=';',)
+bombero = np.loadtxt('./ActuacionesBomberos_2025.csv', skiprows=1, delimiter=';',)
 FENTANILO
 PERRO SANCHEZ
