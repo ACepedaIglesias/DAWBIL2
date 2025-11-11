0b9b0c47e8d530698bafddbb9c0126ea939816bd
JeanpaulDAW <jean.milachay04@gmail.com>
2025-11-11 11:07:15 +0100

    Update trabajonumpy.py

--- trabajonumpy.py
+++ trabajonumpy.py
@@ -3,3 +3,4 @@ import matplotlib.pyplot as plt
 import numpy as np
 #leemos el iris, cargamos a matriz, descartamos primera fila, separador ,
 bombero = np.loadtxt('.ActuacionesBomberos_2025.csv', skiprows=1, delimiter=';',)
+FENTANILO
