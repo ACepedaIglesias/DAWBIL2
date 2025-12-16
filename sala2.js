3856d93bd94d1da12ab423d6461958b930c67c74
ACepedaIglesias <acepedaiglesias@gmail.com>
2025-12-16 23:38:49 +0100

    Crear Sala 2 JS
    - Creacion del JS para la sala 2

---
+++ sala2.js
@@ -0,0 +1,10 @@
+// Usa Map, Set, Array.from, spread/destructuring y varios métodos de Array
+
+// Map con fragmentos que forman la palabra objetivo: "LIBRO"
+const piezas = new Map([[1, 'LI'], [2, 'BR'], [3, 'O']]);
+
+// Mostrar fragmentos en la pantalla en orden aleatorio
+const cont = document.getElementById('fragments');
+cont.innerHTML = '';
+
+
